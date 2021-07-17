d41c1f5db3370950f25923d05436b5a05d80d0bd
jusrebollo <jrebollo1@fordham.edu>
2021-07-17 17:23:08 -0400

    Update lake_mead_dashboard.py

--- app/lake_mead_dashboard.py
+++ app/lake_mead_dashboard.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from flask import app
 
 from dash.dependencies import Input, Output
 
